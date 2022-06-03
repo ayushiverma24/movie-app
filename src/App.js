c86c3b77b25bda0685e50a5418f833b5fa211395
ayushiverma24 <vayushi121@gmail.com>
2022-06-03 11:51:38 +0530

    worked on Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
-
+import './App.scss';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import MovieDetails from './components/MovieDetails/MovieDetails'
+import PageNotFound from './components/PageNotFound/PageNotFound'
+import Footer from './components/Footer/Footer'
 function App() {
   return (
-    <div >
-      <h1>Hello World!</h1>
-      HIHIHIHIIh
+    <div className='app'>
+      <Router>
+      <Header />
+       <Routes>
+          <Route exact path='/' element={<Home/>}></Route>
+          <Route path='/movie/:imdbID' element={<MovieDetails/>}></Route>
+          <Route exact path='/404' element={<PageNotFound/>}></Route>
+        </Routes>
+        <Footer/>
+      </Router>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './Header.scss'
+import userImage from '../../Images/userImage.png'
 const Header=()=>{
     return (
-        <div>
-            Header
-        </div>
+        <div className='header'>
+           <Link to='/'>
+           <div className='logo'>Logo</div>
+           </Link>
+           <div className='user-image'>
+                <img src={userImage} alt='user-image' />
+                
+           </div>
+
+        </div> 
     )
 }
 
