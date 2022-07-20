9262c60addaf9f85eb2eb20f77bc73f5cba9de5e
ayushiverma24 <vayushi121@gmail.com>
2022-07-21 00:15:29 +0530

    stored in redux

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,24 +2,27 @@ import React, { useEffect } from 'react';
 import movieApi from '../../common/apis/movieApi';
 import MovieListing from '../MovieListing/MovieListing';
 import { APIKey } from '../../common/apis/movieApiKey';
+import { addMovies } from '../../features/movies/movieSlice';
+import { useDispatch } from 'react-redux/es/exports';
 const Home = () => {
 
     const movieText = 'Harry';
+    const dispatch = useDispatch();
     const fetchMovies = async () => {
         const resp = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
-        .catch(err=>{
-            console.log("got error", err);
-        })
-        console.log("Response =  ",resp);
+            .catch(err => {
+                console.log("got error", err);
+            })
+        dispatch(addMovies(resp.data))
     }
 
     useEffect(() => {
-fetchMovies();
+        fetchMovies();
     }, [])
     return (
         <div>
             <div>
-                <img></img>
+                {/* <img></img> */}
             </div>
             <MovieListing />
         </div>
--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,6 +1,11 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux/es/hooks/useSelector'
+import { getAllMovies } from '../../features/movies/movieSlice'
 const MovieListing =()=>{
+
+    const movies  = useSelector(getAllMovies);
+    console.log("movies  = ",movies);
+
     return(
         <div>
             MovieListing
